513d0ce671d9004a9a49c3fee6ad0d0f2b3a41dd
sizet <sizety@163.com>
2025-05-25 21:47:14 +0800

    fix: 导入 .ts 文件报错

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,17 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
+	settings: {
+		'import/resolver': {
+			typescript: {
+				// 指定 tsconfig.json 路径（默认会查找项目根目录的 tsconfig.json）
+				project: './tsconfig.json',
+			},
+			// 如果你使用了路径别名（如 `@/*`），还需要 node 解析器
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
 	env: {
 		browser: true,
 		commonjs: true,
